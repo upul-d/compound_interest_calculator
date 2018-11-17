25758c678abdf762da83f4f1a973a543d8ab9ba7
Upul Dissanayaka <u.dissanayaka@gmail.com>
2018-11-17 23:05:48 +0000

    Adds React component - Calculator

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react';
 import './App.css';
+import Calculator from './components/Calculator';
 
 class App extends Component {
   render() {
-    return <h1>Compound Interest Calculator</h1>;
+    return <Calculator title='Compound Interest Calculator'/>;
   }
 }
 
