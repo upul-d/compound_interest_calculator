becfd7b3e237fc87322d0025e94413f9d7da98b9
Upul Dissanayaka <u.dissanayaka@gmail.com>
2018-11-18 12:17:55 +0000

    Adds ResultView heading

--- src/components/ResultView.js
+++ src/components/ResultView.js
@@ -3,6 +3,7 @@ import React from 'react';
 const ResultView = ({balance, initialAmountForResultView, amountFromInterest}) => {
   return (
     <div>
+    <h2>Your Result</h2>
     <p>Balance: £{balance}</p>
     <p>Initial Amount (Principal): £{initialAmountForResultView}</p>
     <p>Amount From Interest: £{amountFromInterest}</p>
