21a67a6c47c69935d312727bc7b95d5d9871626f
Upul Dissanayaka <u.dissanayaka@gmail.com>
2018-11-18 00:04:23 +0000

    Adds form to submit values for interest calculation

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -4,14 +4,45 @@ import ResultView from './ResultView';
 class Calculator extends Component {
   constructor(props) {
     super(props)
-    this.state = {};
+    this.state = {
+      initialAmount: '',
+      interestRate: '',
+      numberOfYears: '',
+      balance: ''
+    };
   }
 
   render() {
     return <div>
       <h1>{this.props.title}</h1>
-      {/* ResultView given temporarily hard-coded balance*/}
-      <ResultView balance='10.00'/>
+      <form className="compound-interest-form">
+        <input
+          type="number"
+          placeholder="Initial Amount"
+          min="1"
+          required
+        />
+        <br/>
+        <input
+          type="number"
+          placeholder="Interest Rate"
+          min="0.01"
+          step="any"
+          required
+        />
+        <br/>
+        <input
+          type="number"
+          placeholder="Number of Years"
+          min="1"
+          step="any"
+          required
+        />
+        <br/>
+        <input type="submit" value="Calculate Compound Interest" />
+        <br/>
+      </form>
+      <ResultView balance={this.state.balance}/>
     </div>
   }
 }
