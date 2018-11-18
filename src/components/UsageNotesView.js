d4ab1c041c2a40b7c0a365be8d342d2a4debc01e
Upul Dissanayaka <u.dissanayaka@gmail.com>
2018-11-18 21:56:46 +0000

    Reduces font size of usage notes (supplementary information)

--- src/components/UsageNotesView.js
+++ src/components/UsageNotesView.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const UsageNotesView = () => {
   return (
-    <div className="usage-notes-container">
+    <div className="usage-notes-wrapper">
       <h3 className="usage-notes-header">Please Note</h3>
       <ul className="usage-notes-list">
         <li>This calculator assumes that interest is compounded yearly</li>
