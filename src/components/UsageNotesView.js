925d35a57c329850a257b630cd4a751f523d4e5a
Upul Dissanayaka <u.dissanayaka@gmail.com>
2018-11-19 05:21:27 +0000

    Renders rounding info in UsageNotesView

--- src/components/UsageNotesView.js
+++ src/components/UsageNotesView.js
@@ -6,6 +6,7 @@ const UsageNotesView = () => {
       <h3 className="usage-notes-header">Please Note</h3>
       <ul className="usage-notes-list">
         <li>This calculator assumes that interest is compounded yearly</li>
+        <li>Relevant input values and results are rounded to two decimal places</li>
       </ul>
     </div>
   );
