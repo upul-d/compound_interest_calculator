5f6299633696817bea32dd1b2f9f3391760a3b94
Upul Dissanayaka <u.dissanayaka@gmail.com>
2018-11-19 00:57:50 +0000

    Adds ability to handle invalid input which is not handled by html form validation

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -17,7 +17,12 @@ class Calculator extends Component {
         interestRateForResultView: null,
         numberOfYearsForResultView: null
       },
-      amountFromInterest: ''
+      amountFromInterest: '',
+      errorsForInputs: {
+        initialAmount: '',
+        interestRate: '',
+        numberOfYears: ''
+      }
     };
 
     this.handleInitialAmountChange = this.handleInitialAmountChange.bind(this);
@@ -27,15 +32,51 @@ class Calculator extends Component {
   }
 
   handleInitialAmountChange(event) {
-    this.setState({initialAmount: event.target.value});
+    const validInput = calculator.isValidInput(event.target.value);
+    if (validInput) {
+      this.setState({initialAmount: event.target.value});
+      return;
+    }
+    const error = 'Please enter a positive number';
+    this.setState(
+      {errorsForInputs:
+        {
+          initialAmount: error
+        }
+      }
+    );
   }
 
   handleInterestRateChange(event) {
-    this.setState({interestRate: event.target.value});
+    const validInput = calculator.isValidInput(event.target.value);
+    if (validInput) {
+      this.setState({interestRate: event.target.value});
+      return;
+    }
+    const error = 'Please enter a positive number';
+    this.setState(
+      {errorsForInputs:
+        {
+          interestRate: error
+        }
+      }
+    );
   }
 
   handleNumberOfYearsChange(event) {
-    this.setState({numberOfYears: event.target.value});
+    const validInput = calculator.isValidInput(event.target.value);
+    if (validInput) {
+      this.setState({numberOfYears: event.target.value});
+      return;
+    }
+    const error = 'Please enter a positive number';
+    this.setState(
+      {errorsForInputs:
+        {
+          numberOfYears: error
+        }
+      }
+    );
   }
 
   handleSubmit(event) {
@@ -61,7 +102,12 @@ class Calculator extends Component {
           interestRateForResultView: Number.parseFloat(interestRate).toFixed(2),
           numberOfYearsForResultView: numberOfYears
         },
-        amountFromInterest: amountFromInterest
+        amountFromInterest: amountFromInterest,
+        errorsForInputs: {
+          initialAmount: '',
+          interestRate: '',
+          numberOfYears: ''
+        },
       }
     );
   }
@@ -71,53 +117,56 @@ class Calculator extends Component {
       <div>
         <h1>{this.props.title}</h1>
         <div className="calculator-wrapper">
-        <h2>Calculator</h2>
-        <p className="above-form-instruction">Please Enter Your Values Below:</p>
-        <form className="compound-interest-form" onSubmit={this.handleSubmit}>
-          <div className="input-wrapper">
-            <label>
-              Initial Amount (Principal)
-              <p className="input-hint">Minimum: £1</p>
-              <input
-                id="amount"
-                type="number"
-                min="1"
-                step="any"
-                required
-                value={this.state.initialAmount}
-                onChange={this.handleInitialAmountChange}
-              />
-            </label>
-          </div>
-          <div className="input-wrapper">
-            <label>
-              Interest Rate (%)
-              <p className="input-hint">Minimum: 0.01%</p>
-              <input
-                type="number"
-                min="0.01"
-                step="any"
-                required
-                value={this.state.interestRate}
-                onChange={this.handleInterestRateChange}
-              />
-            </label>
-          </div>
-          <div className="input-wrapper">
-            <label>
-              Number of Years
-              <p className="input-hint">Whole years only</p>
-              <input
-                type="number"
-                min="1"
-                required
-                value={this.state.numberOfYears}
-                onChange={this.handleNumberOfYearsChange}
-              />
-            </label>
-          </div>
-          <input id="calculate-button" type="submit" value="Calculate" />
-        </form>
+          <h2>Calculator</h2>
+          <p className="above-form-instruction">Please Enter Your Values Below:</p>
+          <form className="compound-interest-form" onSubmit={this.handleSubmit}>
+            <div className="input-wrapper">
+              <label>
+                Initial Amount (Principal)
+                <p className="input-hint">Minimum: £1</p>
+                <p className="input-error-display">{this.state.errorsForInputs.initialAmount}</p>
+                <input
+                  id="amount"
+                  type="number"
+                  min="1"
+                  step="any"
+                  required
+                  value={this.state.initialAmount}
+                  onChange={this.handleInitialAmountChange}
+                />
+              </label>
+            </div>
+            <div className="input-wrapper">
+              <label>
+                Interest Rate (%)
+                <p className="input-hint">Minimum: 0.01%</p>
+                <p className="input-error-display">{this.state.errorsForInputs.interestRate}</p>
+                <input
+                  type="number"
+                  min="0.01"
+                  step="any"
+                  required
+                  value={this.state.interestRate}
+                  onChange={this.handleInterestRateChange}
+                />
+              </label>
+            </div>
+            <div className="input-wrapper">
+              <label>
+                Number of Years
+                <p className="input-hint">Whole years only</p>
+                <p className="input-error-display">{this.state.errorsForInputs.numberOfYears}</p>
+                <input
+                  type="number"
+                  min="1"
+                  required
+                  value={this.state.numberOfYears}
+                  onChange={this.handleNumberOfYearsChange}
+                />
+              </label>
+            </div>
+            <input id="calculate-button" type="submit" value="Calculate" />
+          </form>
         </div>
         <ResultView
           balance={this.state.balance}
